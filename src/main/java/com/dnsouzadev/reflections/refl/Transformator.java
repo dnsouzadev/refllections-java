dd125ca1d0bf523cdf9cb9f81571b0669ec2372f
Daniel Souza <workdndsza@gmail.com>
2024-09-05 17:54:47 -0300

    instanciando a classe

--- src/main/java/com/dnsouzadev/reflections/refl/Transformator.java
+++ src/main/java/com/dnsouzadev/reflections/refl/Transformator.java
@@ -1,9 +1,13 @@
 package com.dnsouzadev.reflections.refl;
 
+import java.lang.reflect.InvocationTargetException;
+
 public class Transformator {
 
-    public <I, O> transform(I input) throws ClassNotFoundException {
+    public <I, O> O transform(I input) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
         Class<?> source = input.getClass();
-        Class<?> target = source.forName(source.getClass() + "DTO");
+        Class<?> target = source.forName(source.getName() + "DTO");
+
+        return (O) target.getDeclaredConstructor().newInstance();
     }
 }
