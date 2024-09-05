3c63d27a56783777b1f0ee8eb5a1dc9ba43fe4cd
Daniel Souza <workdndsza@gmail.com>
2024-09-05 17:59:33 -0300

    pegando os campos de uma classe

--- src/main/java/com/dnsouzadev/reflections/refl/Transformator.java
+++ src/main/java/com/dnsouzadev/reflections/refl/Transformator.java
@@ -1,13 +1,19 @@
 package com.dnsouzadev.reflections.refl;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 
 public class Transformator {
 
     public <I, O> O transform(I input) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
         Class<?> source = input.getClass();
-        Class<?> target = source.forName(source.getName() + "DTO");
+        Class<?> target = Class.forName(source + "DTO");
 
-        return (O) target.getDeclaredConstructor().newInstance();
+        O targetClass = (O) target.getDeclaredConstructor().newInstance();
+
+        Field[] sourceFields = source.getDeclaredFields();
+        Field[] targetFields = target.getDeclaredFields();
+
+        return targetClass;
     }
 }
